f9ed12da40a15a3b5570b029960c32d2a0b8914d
diegoprogrammer89 <diegodaod@gmail.com>
2023-01-10 15:55:00 -0400

    commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+import { useState, useEffect } from 'react'
+import axios from "axios"
+import Drinks from './components/Drinks'
+
+
+
+function App() {
+ 
+  const  [ drinkData , setDrinkData ] = useState ({})
+
+
+  useEffect(() => {
+    axios
+    .get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+    .then( (resp) => setDrinkData(resp.data) )
+    .catch( (error) => console.error(error) )
+    
+  }, [] )
+
+
+
+
+
+  return (
+    <div className="App">
+        <Drinks data = {drinkData}/>  
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Drinks.jsx
@@ -0,0 +1,23 @@
+
+
+const Drinks = ({data}) => {
+    console.log(data);
+     return (
+        <article>
+
+            
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h3>nombre:</h3>
+               
+
+            </div>
+
+        </article>
+     )
+}
+
+
+export default Drinks
